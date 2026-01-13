154f04679ddeba0b29d1bf2febcb1650c53b5cdb
Infinity-MaxX <katerina@oestergaard.com>
2026-01-13 18:16:44 +0100

    Finished and tested the deleteListElement function.

--- js/index.js
+++ js/index.js
@@ -21,6 +21,19 @@ function changeClass(element, value) {
     return element;
 }
 
+function deleteListElement(nodeId, tagName, elementId) {
+    // get the parent node from which the element belongs to
+    const node = document.getElementById(nodeId);
+    // get a list of all children within the parent with a specific tag name
+    const liElement = node.getElementsByTagName(tagName);
+    // select the child at the specific position
+    const element = liElement[elementId];
+    // remove the child
+    node.removeChild(element);
+
+    return node;
+}
+
 // Test functions
 const newNode = createNode("list", "healthy", "Pears");
 //console.log(newNode);
@@ -31,3 +44,5 @@ const addToList = appendToList("list", newNode);
 //const newClass = changeClass(newNode, "unhealthy");
 //console.log(newClass);
 
+//const removeMarshmallows = deleteListElement("list", "li", "0");
+//console.log(removeMarshmallows);
