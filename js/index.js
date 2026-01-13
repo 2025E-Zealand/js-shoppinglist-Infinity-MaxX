fc51924db1cc2bafaa8bdd556d9d998b9efd1ff9
Infinity-MaxX <katerina@oestergaard.com>
2026-01-13 16:47:38 +0100

    Finished and tested createNode and appendToList.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,27 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+// Functions
+function createNode(idAtt, classAtt, textAtt) {
+    const newNode = document.createElement("li");
+    newNode.setAttribute("id", idAtt);
+    newNode.setAttribute("class", classAtt);
+    const textNode = document.createTextNode(textAtt);
+    newNode.appendChild(textNode);
+
+    return newNode;
+}
+
+function appendToList(id, node) {
+    return document.getElementById(id).appendChild(node);
+}
+
+
+
+// Test functions
+const textElem = createNode("list", "healthy", "Pears");
+//console.log(textElem);
+
+const addToList = appendToList("list", textElem);
+//console.log(addToList);
+
